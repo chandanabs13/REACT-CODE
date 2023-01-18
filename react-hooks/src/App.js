e21bb881ca602be612c2ed7ba0ce50b8b4e5567d
List <chandana.bs@awign.com>
2023-01-18 20:43:12 +0530

    useState

---
+++ react-hooks/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+// import UseState from '../component/usestate-hook';
+import UseStateHook from './component/UseStateHook';
+
+function App() {
+  return (
+    <div className="App">
+      <UseStateHook />
+    </div>
+  );
+}
+
+export default App;
